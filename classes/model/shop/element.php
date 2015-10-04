3ac1da9795dfc72d56d511dfa5994fe424269563
gregor <gregor.zoonoid@gmail.com>
2015-10-04 22:10:40 +0300

    Relationship from element to nomenclature
    Signed-off-by: gregor <gregor.zoonoid@gmail.com>

--- classes/model/shop/element.php
+++ classes/model/shop/element.php
@@ -1,3 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Shop_Element extends Model_Catalog_Element {}
+class Model_Shop_Element extends Model_Catalog_Element {
+	
+	protected function _initialize()
+	{
+		parent::_initialize();
+		
+		$this->_has_many['nomenclatures'] = array(
+			'model' => 'shop_Nomenclature',
+			'foreign_key' => 'product_id',
+		);
+	}
+	
+}
