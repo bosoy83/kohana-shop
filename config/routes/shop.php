6f6b3cb356c79a028315f30aadfa2a8f88914196
gregor <gregor.zoonoid@gmail.com>
2015-10-28 22:45:41 +0300

    Route for element
    Signed-off-by: gregor <gregor.zoonoid@gmail.com>

--- config/routes/shop.php
+++ config/routes/shop.php
@@ -1,7 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array (
-	'shop' => array(
+	'shop_element' => array(
+		'uri_callback' => '/<element_uri>.html(?<query>)',
+		'defaults' => array(
+			'directory' => 'modules',
+			'controller' => 'shop',
+			'action' => 'detail',
+		)
+	),
+	'shop_category' => array(
 		'uri_callback' => array('Helper_Shop', 'route'), 
 		'regex' => '(/<category_uri>(/<element_uri>.html))(?<query>)',
 		'defaults' => array(
